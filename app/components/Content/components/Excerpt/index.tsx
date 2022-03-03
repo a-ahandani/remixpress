9b23023df516c252cb34242d0224bf268de811ae
Ahmad <a.e.ahandani@gmail.com>
2022-03-03 13:27:17 -0800

    feat: format date

--- app/components/Content/components/Excerpt/index.tsx
+++ app/components/Content/components/Excerpt/index.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import dayjs from "dayjs";
 import Body from "~/components/Content/components/Body";
 import Title from "~/components/Content/components/Title";
@@ -16,10 +16,13 @@ export default function Excerpt({
   var day = dayjs(date);
   return (
     <Box sx={{ mt: 4, ml: 3 }}>
+      <Typography variant="caption">
+        {day.format("dddd, MMM DD, YYYY")}
+      </Typography>
+      <br />
       <Title to={slug}>{title}</Title>
       <Body>{excerpt}</Body>
       {commentCount && `comments: ${commentCount}`}
-      date:{day.format("DD/MM/YYYY")}
     </Box>
   );
 }
