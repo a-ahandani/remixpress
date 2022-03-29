b812b2e6de7ddd110106f53ded8efcf74c111a24
Ahmad <a.e.ahandani@gmail.com>
2022-03-29 15:30:22 -0700

    fix: fix package.json

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,6 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: "cloudflare-pages",
-  server: "./server.js",
-  devServerBroadcastDelay: 1000,
   ignoredRouteFiles: [".*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
